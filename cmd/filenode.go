b017370921b347819b41956e40d78e8ac0c07031
Sergey Cherepanov <cheggaaa@gmail.com>
2023-06-07 14:19:05 +0200

    update any-sync, yamux config

--- cmd/filenode.go
+++ cmd/filenode.go
@@ -15,10 +15,11 @@ import (
 	"github.com/anyproto/any-sync/coordinator/coordinatorclient"
 	"github.com/anyproto/any-sync/coordinator/nodeconfsource"
 	"github.com/anyproto/any-sync/metric"
-	"github.com/anyproto/any-sync/net/dialer"
+	"github.com/anyproto/any-sync/net/peerservice"
 	"github.com/anyproto/any-sync/net/pool"
 	"github.com/anyproto/any-sync/net/rpc/server"
 	"github.com/anyproto/any-sync/net/secureservice"
+	"github.com/anyproto/any-sync/net/transport/yamux"
 	"github.com/anyproto/any-sync/nodeconf"
 	"github.com/anyproto/any-sync/nodeconf/nodeconfstore"
 	"go.uber.org/zap"
@@ -103,15 +104,17 @@ func Bootstrap(a *app.App) {
 		Register(nodeconfsource.New()).
 		Register(nodeconfstore.New()).
 		Register(nodeconf.New()).
-		Register(dialer.New()).
+		Register(peerservice.New()).
 		Register(secureservice.New()).
+		Register(yamux.New()).
 		Register(pool.New()).
 		Register(coordinatorclient.New()).
 		Register(limit.New()).
 		Register(store()).
 		Register(redisprovider.New()).
 		Register(redisindex.New()).
-		Register(filenode.New()).
+		Register(metric.New()).
 		Register(server.New()).
-		Register(metric.New())
+		Register(filenode.New())
+
 }
--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 	"github.com/anyproto/any-sync/app"
 	"github.com/anyproto/any-sync/metric"
 	"github.com/anyproto/any-sync/net"
+	"github.com/anyproto/any-sync/net/transport/yamux"
 	"github.com/anyproto/any-sync/nodeconf"
 	"gopkg.in/yaml.v3"
 	"os"
@@ -29,6 +30,7 @@ func NewFromFile(path string) (c *Config, err error) {
 type Config struct {
 	Account                  commonaccount.Config   `yaml:"account"`
 	GrpcServer               net.Config             `yaml:"grpcServer"`
+	Yamux                    yamux.Config           `yaml:"yamux"`
 	Metric                   metric.Config          `yaml:"metric"`
 	S3Store                  s3store.Config         `yaml:"s3Store"`
 	FileDevStore             FileDevStore           `yaml:"fileDevStore"`
@@ -80,4 +82,8 @@ func (c Config) GetNodeConfStorePath() string {
 
 func (c Config) GetNodeConfUpdateInterval() int {
 	return c.NetworkUpdateIntervalSec
+}
+
+func (c Config) GetYamux() yamux.Config {
+	return c.Yamux
 }
