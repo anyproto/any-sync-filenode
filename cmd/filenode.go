ba4ccc4428d1a9dafadea8f5e65df68d311e1036
Sergey Cherepanov <cheggaaa@gmail.com>
2023-09-01 17:39:42 +0200

    update proto version

--- cmd/filenode.go
+++ cmd/filenode.go
@@ -67,9 +67,6 @@ func main() {
 	ctx := context.Background()
 	a := new(app.App)
 
-	// set the proto version
-	secureservice.ProtoVersion = 1
-
 	// open config file
 	conf, err := config.NewFromFile(*flagConfigFile)
 	if err != nil {
